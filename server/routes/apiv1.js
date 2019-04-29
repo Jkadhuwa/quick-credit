c2a51199e5b7f0a8429d9a3f877d742bb2387f87
Musinda Kadhuwa <justinemsinda@gmail.com>
2019-04-29 08:37:44 +0300

    [Chore #165668327] serve front end pages on v1 API

--- server/routes/apiv1.js
+++ server/routes/apiv1.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import path from 'path';
 
 const v1 = express.Router();
+
+v1.use(express.static('UI'));
+v1.use('/UI', express.static(path.resolve(__dirname, '../../UI/')));
 
 export default v1;

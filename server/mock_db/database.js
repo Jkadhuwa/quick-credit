bec311d43ba4d64d46d94118d3dd657f39ede946
Musinda Kadhuwa <justinemsinda@gmail.com>
2019-05-08 11:00:38 +0300

    [Feature #165815458] Create loan repayment record endpoint

--- server/mock_db/database.js
+++ server/mock_db/database.js
@@ -1,106 +1,109 @@
-/* eslint-disable indent */
+
 module.exports = {
-  users: [
-    {
-      token: 'SBgpaZdIDdWlHUexc45m',
-      id: 1,
-      email: 'jaymusinda@live.com',
-      firstName: 'Musinda',
-      lastName: 'Kadhuwa',
-      password: 'Kadhush',
-      address: 'Clay works appt 4, Seasons st Kasarani',
-      nationality: 'Kenyan',
-      telephone: '+254713723191',
-      workAddress: 'bruce hse Nrb',
-      status: 'verified',
-      isAdmin: true
-    },
-    {
-      token: 'SBgpaZdIDdWlHUexc46m',
-      id: 2,
-      email: 'joankadzo@gmail.com',
-      firstName: 'Joan',
-      lastName: 'Kadzo',
-      password: 'Joankadzo',
-      address: 'Majengo plt No 415, Malindi',
-      nationality: 'Rwandan',
-      telephone: '+250713723367',
-      workAddress: 'KIST hse kgl',
-      status: 'verified',
-      isAdmin: true
-    },
-    {
-      token: 'SBgpaZdIDdWlHUexc4m',
-      id: 3,
-      email: 'sam3ziro@gmail.com',
-      firstName: 'Samuel',
-      lastName: 'Ziro',
-      password: 'Kadhush',
-      address: 'GSU Barracks, Embakasi',
-      nationality: 'Kenyan',
-      telephone: '+254713723151',
-      workAddress: 'Embakasi gsu Hqs',
-      status: 'unverified',
-      isAdmin: false
-    },
-    {
-      id: 4,
-      email: 'kja2aro@gmail.com',
-      firstName: 'James',
-      lastName: 'Saro',
-      password: 'kalume!',
-      address: 'Clay works appt 6, Seasons st Kasarani',
-      nationality: 'Kenyan',
-      telephone: '+254713723191',
-      workAddress: 'bruce hse Nrb',
-      status: 'verified',
-      isAdmin: false
-    }
-  ],
-  loans: [
-    {
-      loanId: 1,
-      user: 'sam3ziro@gmail.com',
-      createdOn: '12-10-2019',
-      status: 'Pending',
-      repaid: false,
-      tenor: 10,
-      loanAmount: 50000,
-      balance: 50000,
-      paymentInstallment: 5250,
-      interest: 2500
-    },
-    {
-      loanId: 2,
-      user: 'joankadzo@gmail.com',
-      createdOn: '12-10-2019',
-      status: 'approved',
-      repaid: false,
-      tenor: 10,
-      amount: 10000,
-      balance: 5000,
-      paymentInstallment: 1050,
-      interest: 500
-    },
-    {
-      loanId: 3,
-      user: 'kja2aro@gmail.com',
-      createdOn: '12-10-2019',
-      status: 'rejected',
-      repaid: false,
-      tenor: 10,
-      amount: 10000,
-      balance: 0,
-      paymentInstallment: 1050,
-      interest: 500
-    }
-  ],
-  repayments: [
-    {
-      id: 2,
-      createdOn: '13-12-2019',
-      loanId: 1,
-      amount: 5000
-    }
-  ]
+	users: [
+		{
+			token: 'SBgpaZdIDdWlHUexc45m',
+			id: 1,
+			email: 'jaymusinda@live.com',
+			firstName: 'Musinda',
+			lastName: 'Kadhuwa',
+			password: 'Kadhush',
+			address: 'Clay works appt 4, Seasons st Kasarani',
+			nationality: 'Kenyan',
+			telephone: '+254713723191',
+			workAddress: 'bruce hse Nrb',
+			status: 'verified',
+			isAdmin: true
+		},
+		{
+			token: 'SBgpaZdIDdWlHUexc46m',
+			id: 2,
+			email: 'joankadzo@gmail.com',
+			firstName: 'Joan',
+			lastName: 'Kadzo',
+			password: 'Joankadzo',
+			address: 'Majengo plt No 415, Malindi',
+			nationality: 'Rwandan',
+			telephone: '+250713723367',
+			workAddress: 'KIST hse kgl',
+			status: 'verified',
+			isAdmin: true
+		},
+		{
+			token: 'SBgpaZdIDdWlHUexc4m',
+			id: 3,
+			email: 'sam3ziro@gmail.com',
+			firstName: 'Samuel',
+			lastName: 'Ziro',
+			password: 'Kadhush',
+			address: 'GSU Barracks, Embakasi',
+			nationality: 'Kenyan',
+			telephone: '+254713723151',
+			workAddress: 'Embakasi gsu Hqs',
+			status: 'unverified',
+			isAdmin: false
+		},
+		{
+			id: 4,
+			email: 'kja2aro@gmail.com',
+			firstName: 'James',
+			lastName: 'Saro',
+			password: 'kalume!',
+			address: 'Clay works appt 6, Seasons st Kasarani',
+			nationality: 'Kenyan',
+			telephone: '+254713723191',
+			workAddress: 'bruce hse Nrb',
+			status: 'verified',
+			isAdmin: false
+		}
+	],
+	loans: [
+		{
+			loanId: 1,
+			user: 'sam3ziro@gmail.com',
+			createdOn: '12-10-2019',
+			status: 'Pending',
+			repaid: false,
+			tenor: 10,
+			amount: 50000,
+			totalAmount: 52500,
+			balance: 52500,
+			paymentInstallment: 5250,
+			interest: 2500
+		},
+		{
+			loanId: 2,
+			user: 'joankadzo@gmail.com',
+			createdOn: '12-10-2019',
+			status: 'approved',
+			repaid: false,
+			tenor: 10,
+			amount: 10000,
+			totalAmount: 10500,
+			balance: 5000,
+			paymentInstallment: 1050,
+			interest: 500
+		},
+		{
+			loanId: 3,
+			user: 'kja2aro@gmail.com',
+			createdOn: '12-10-2019',
+			status: 'rejected',
+			repaid: false,
+			tenor: 10,
+			totalAmount: 10500,
+			amount: 10000,
+			balance: 0,
+			paymentInstallment: 1050,
+			interest: 500
+		}
+	],
+	repayments: [
+		{
+			id: 1,
+			createdOn: '13-12-2019',
+			loanId: 1,
+			amount: 5000
+		}
+	]
 };
--- server/test/loan.test.js
+++ server/test/loan.test.js
@@ -9,157 +9,203 @@ chai.use(chaiHttp);
 // CREATE /SIGN UP TESTS
 
 describe('User applies a loan', () => {
-  it('Should return status 201 with data of newly created loan application', (done) => {
-    chai
-      .request(app)
-      .post('/api/v1/loans')
-      .set('access-token', 'SBgpaZdIDdWlHUexc46m')
-      .send({
-        amount: '150000',
-        tenor: '12'
-      })
-      .end((err, res) => {
-        expect(res).to.have.status(201);
-        expect(res.body.data).to.be.an('object');
-        done();
-      });
-  });
+	it('Should return status 201 with data of newly created loan application', (done) => {
+		chai
+			.request(app)
+			.post('/api/v1/loans')
+			.set('access-token', 'SBgpaZdIDdWlHUexc46m')
+			.send({
+				amount: '150000',
+				tenor: '12'
+			})
+			.end((err, res) => {
+				expect(res).to.have.status(201);
+				expect(res.body.data).to.be.an('object');
+				done();
+			});
+	});
 });
 describe('Admin view  all loans', () => {
-  it('Should return status 200 with data all loan appliocations', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/loans')
-      .set('access-token', 'SBgpaZdIDdWlHUexc46m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.property('data');
-        done();
-      });
-  });
+	describe('Return all Loans when successful', () => {
+		it('Should return status 200 with data all loan appliocations', (done) => {
+			chai
+				.request(app)
+				.get('/api/v1/loans')
+				.set('access-token', 'SBgpaZdIDdWlHUexc46m')
+				.send()
+				.end((err, res) => {
+					expect(res).to.have.status(200);
+					expect(res.body).to.have.property('data');
+					done();
+				});
+		});
+	});
+	it('Should return status 401 with error Invalid token', (done) => {
+		chai
+			.request(app)
+			.get('/api/v1/loans')
+			.end((err, res) => {
+				expect(res).to.have.status(401);
+				done();
+			});
+	});
 
-  it('Should return status 401 with error Invalid token', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/loans')
-      .end((err, res) => {
-        expect(res).to.have.status(401);
-        done();
-      });
-  });
-
-  it('Should return status 401 with error Only previledged users can view for normal users', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/loans')
-      .set('access-token', 'SBgpaZdIDdWlHUexc4m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(401);
-        expect(res.body.error).to.equal('Only previledged users can view ');
-        done();
-      });
-  });
+	it('Should return status 401 with error Only previledged users can view for normal users', (done) => {
+		chai
+			.request(app)
+			.get('/api/v1/loans')
+			.set('access-token', 'SBgpaZdIDdWlHUexc4m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(401);
+				expect(res.body.error).to.equal('Only previledged users can view ');
+				done();
+			});
+	});
 });
 
-// Adnin tests
+// Admin tests
 
 describe('Admin should view  specific loan', () => {
-  it('Should return status 200 with data  loan appliocations of specified LoanId', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/loans/1')
-      .set('access-token', 'SBgpaZdIDdWlHUexc46m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.property('data');
-        done();
-      });
-  });
+	it('Should return status 200 with data  loan appliocations of specified LoanId', (done) => {
+		chai
+			.request(app)
+			.get('/api/v1/loans/1')
+			.set('access-token', 'SBgpaZdIDdWlHUexc46m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res.body).to.have.property('data');
+				done();
+			});
+	});
 
-  it('Should return status 404 if no loan with the specied id is found', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/loans/0')
-      .set('access-token', 'SBgpaZdIDdWlHUexc46m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        expect(res.body).to.have.property('error');
-        expect(res.body.error).to.be.equal('Loan not found ');
-        done();
-      });
-  });
-  it('Should return status 404 if no loan with the specied id is found', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/loans/0')
-      .set('access-token', 'SBgpaZdIDdWlHUexc46m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        expect(res.body).to.have.property('error');
-        expect(res.body.error).to.be.equal('Loan not found ');
-        done();
-      });
-  });
+	it('Should return status 404 if no loan with the specied id is found', (done) => {
+		chai
+			.request(app)
+			.get('/api/v1/loans/0')
+			.set('access-token', 'SBgpaZdIDdWlHUexc46m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(404);
+				expect(res.body).to.have.property('error');
+				expect(res.body.error).to.be.equal('Loan not found ');
+				done();
+			});
+	});
+	it('Should return status 404 if no loan with the specied id is found', (done) => {
+		chai
+			.request(app)
+			.get('/api/v1/loans/0')
+			.set('access-token', 'SBgpaZdIDdWlHUexc46m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(404);
+				expect(res.body).to.have.property('error');
+				expect(res.body.error).to.be.equal('Loan not found ');
+				done();
+			});
+	});
 });
 describe('Admin should view be able to Approve or Reject loan', () => {
-  it('Should return status 200 with data of loan applications of specified LoanId', (done) => {
-    chai
-      .request(app)
-      .patch('/api/v1/loans/1')
-      .set('access-token', 'SBgpaZdIDdWlHUexc46m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.property('data');
-        expect(res.body.data).to.have.property('loanId');
-        done();
-      });
-  });
-  it('Should return status 401 with error Wrong token ', (done) => {
-    chai
-      .request(app)
-      .patch('/api/v1/loans/1')
-      .set('access-token', 'SBgpaZdIDdWlHUexc6m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(401);
-        expect(res.body).to.have.property('error');
-        expect(res.body).to.have.property('status');
-        expect(res.body.error).to.be.equal('Token error');
-        done();
-      });
-  });
-  it('Should return status 404 with error Wrong token ', (done) => {
-    chai
-      .request(app)
-      .patch('/api/v1/loans/0')
-      .set('access-token', 'SBgpaZdIDdWlHUexc46m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        expect(res.body).to.have.property('error');
-        expect(res.body).to.have.property('status');
-        expect(res.body.error).to.be.equal('Loan not found ');
-        done();
-      });
-  });
-  it('Should return status 404 with error dont have enough user previledge ', (done) => {
-    chai
-      .request(app)
-      .patch('/api/v1/loans/0')
-      .set('access-token', 'SBgpaZdIDdWlHUexc4m')
-      .send()
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        expect(res.body).to.have.property('error');
-        expect(res.body).to.have.property('status');
+	it('Should return status 200 with data of loan applications of specified LoanId', (done) => {
+		chai
+			.request(app)
+			.patch('/api/v1/loans/1')
+			.set('access-token', 'SBgpaZdIDdWlHUexc46m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res.body).to.have.property('data');
+				expect(res.body.data).to.have.property('loanId');
+				done();
+			});
+	});
+	it('Should return status 401 with error Wrong token ', (done) => {
+		chai
+			.request(app)
+			.patch('/api/v1/loans/1')
+			.set('access-token', 'SBgpaZdIDdWlHUexc6m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(401);
+				expect(res.body).to.have.property('error');
+				expect(res.body).to.have.property('status');
+				expect(res.body.error).to.be.equal('Token error');
+				done();
+			});
+	});
+	it('Should return status 404 with error Wrong token ', (done) => {
+		chai
+			.request(app)
+			.patch('/api/v1/loans/0')
+			.set('access-token', 'SBgpaZdIDdWlHUexc46m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(404);
+				expect(res.body).to.have.property('error');
+				expect(res.body).to.have.property('status');
+				expect(res.body.error).to.be.equal('Loan not found ');
+				done();
+			});
+	});
+	it('Should return status 404 with error dont have enough user previledge ', (done) => {
+		chai
+			.request(app)
+			.patch('/api/v1/loans/0')
+			.set('access-token', 'SBgpaZdIDdWlHUexc4m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(404);
+				expect(res.body).to.have.property('error');
+				expect(res.body).to.have.property('status');
 
-        done();
-      });
-  });
+				done();
+			});
+	});
+});
+
+// Create Loan Repayment Record
+
+describe('User Should be able to create a loan repayment record', () => {
+	it('Should return status 201 with data of loan repayment record', (done) => {
+		chai
+			.request(app)
+			.post('/api/v1/loans/2/repayment')
+			.set('access-token', 'SBgpaZdIDdWlHUexc4m')
+			.send({ amount: '1000' })
+			.end((err, res) => {
+				expect(res).to.have.status(201);
+				expect(res.body).to.have.property('data');
+				expect(res.body.data).to.have.property('loanId');
+				expect(res.body.data.balance).to.be.not.equal(null);
+				done();
+			});
+	});
+	it('Should return status 401 with error message of Token error', (done) => {
+		chai
+			.request(app)
+			.post('/api/v1/loans/1/repayment')
+			.set('access-token', 'SBgpaZdIDdWlexc46m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(401);
+				expect(res.body).to.have.property('error');
+				expect(res.body.error).to.be.equal('Token error');
+				done();
+			});
+	});
+	it('Should return status 404 with error message of Loan Loan Found ', (done) => {
+		chai
+			.request(app)
+			.post('/api/v1/loans/0/repayment')
+			.set('access-token', 'SBgpaZdIDdWlHUexc46m')
+			.send()
+			.end((err, res) => {
+				expect(res).to.have.status(404);
+				expect(res.body).to.have.property('error');
+				expect(res.body.error).to.be.equal('Loan Not Found');
+				done();
+			});
+	});
 });

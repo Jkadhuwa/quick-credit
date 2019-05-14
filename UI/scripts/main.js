fa1c1f43b788e5cef12c9ef8eed2ea33e57e52da
Musinda Kadhuwa <justinemsinda@gmail.com>
2019-05-14 06:34:29 +0300

    [Chore #165966279] Update gh-pages

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -2,13 +2,13 @@
 (function() {
   function toggleSidebar(e) {
     e.preventDefault();
-    const sideBar = document.getElementById("side-nav");
-    sideBar.classList.toggle("side-active");
-    sideBar.classList.toggle("side-hidden");
+    const sideBar = document.getElementById('side-nav');
+    sideBar.classList.toggle('side-active');
+    sideBar.classList.toggle('side-hidden');
   }
-  const menu = document.getElementById("nav-trigger");
+  const menu = document.getElementById('nav-trigger');
   if (menu) {
-    menu.addEventListener("click", toggleSidebar, false);
+    menu.addEventListener('click', toggleSidebar, false);
   }
 })();
 //Preview uploded image by the user
@@ -16,7 +16,7 @@ function showImage() {
   if (this.files && this.files[0]) {
     const obj = new FileReader();
     obj.onload = function(data) {
-      const image = document.getElementById("image");
+      const image = document.getElementById('image');
       image.src = data.target.result;
     };
     obj.readAsDataURL(this.files[0]);
@@ -24,35 +24,35 @@ function showImage() {
 }
 
 //Settings dropdown enabler
-var dropdown = document.getElementsByClassName("dropdown-btn");
+var dropdown = document.getElementsByClassName('dropdown-btn');
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+  dropdown[i].addEventListener('click', function() {
+    this.classList.toggle('active');
     var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
+    if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';
     } else {
-      dropdownContent.style.display = "block";
+      dropdownContent.style.display = 'block';
     }
   });
 }
 
 // Switch sidebar according to screen width(Using ccs media querry)
 
-window.addEventListener("resize", function() {
-  if (window.matchMedia("(min-width: 64.063em)").matches) {
+window.addEventListener('resize', function() {
+  if (window.matchMedia('(min-width: 64.063em)').matches) {
     changeClassName();
   }
 });
 function changeClassName() {
-  const clsName = document.getElementById("side-nav");
+  const clsName = document.getElementById('side-nav');
 
   if (clsName.length > 0) {
-    clsName.className = "side-active side-nav";
+    clsName.className = 'side-active side-nav';
   } else {
-    clsName.className = "side-nav";
+    clsName.className = 'side-nav';
   }
 }
 
@@ -61,13 +61,29 @@ function changeClassName() {
 (function() {
   function toggleUserImg(e) {
     e.preventDefault();
-    const nav_img = document.getElementById("dropdown-menu");
+    const nav_img = document.getElementById('dropdown-menu');
 
-    nav_img.classList.toggle("drop-active");
-    nav_img.classList.toggle("drop-hidden");
+    nav_img.classList.toggle('drop-active');
+    nav_img.classList.toggle('drop-hidden');
   }
-  const img = document.getElementById("user-img");
+  const img = document.getElementById('user-img');
   if (img) {
-    img.addEventListener("click", toggleUserImg, false);
+    img.addEventListener('click', toggleUserImg, false);
   }
 })();
+
+// function to validate input fields
+/*
+const validate = () => {
+  if (document.login.email.value === '') {
+    alert('Please provide your Email!');
+    document.login.email.focus();
+    return false;
+  }
+  if (document.login.password.value === '') {
+    alert('Please provide your password!');
+    document.login.password.focus();
+    return false;
+  }
+};
+*/

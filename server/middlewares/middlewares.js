fb76f946a53b7fe6a88e98b0cd89462816259a08
Musinda Kadhuwa <justinemsinda@gmail.com>
2019-05-21 16:01:25 +0300

    [Feature #166103366] Update middleware.js file

--- server/middlewares/middlewares.js
+++ server/middlewares/middlewares.js
@@ -13,7 +13,7 @@ class Verify {
 			});
 		}
 		try {
-			const token = req.headers.authorization.split(' ')[1];
+			const token = reqToken.split(' ')[1];
 			const verified = jwt.verify(token, process.env.JWT_SECRET);
 			req.userinfo = verified;
 
